b7c8dfd5a44827486bdce6d152f2780ed32b5d51
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-09-11 21:37:10 +0530

    improved the button components

--- client/src/components/btn.jsx
+++ client/src/components/btn.jsx
@@ -1,7 +1,10 @@
-export const BtnPrimary = ({ children, ...props }) => {
+export const BtnPrimary = ({ children, className = "", ...props }) => {
   return (
     <button
-      className="px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-black hover:bg-darkblue"
+      type={props.type || "button"}
+      className={`px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-black hover:bg-darkblue ${
+        props.disabled ? `cursor-not-allowed opacity-50` : ""
+      } ${className}`}
       {...props}
     >
       {children}
@@ -9,10 +12,13 @@ export const BtnPrimary = ({ children, ...props }) => {
   );
 };
 
-export const BtnSecondary = ({ children, ...props }) => {
+export const BtnSecondary = ({ children, className = "", ...props }) => {
   return (
     <button
-      className="px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-yellow hover:bg-white hover:text-yellow"
+      type={props.type || "button"}
+      className={`px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-yellow hover:bg-white hover:text-yellow ${
+        props.disabled ? `cursor-not-allowed opacity-50` : ""
+      } ${className}`}
       {...props}
     >
       {children}
@@ -20,10 +26,13 @@ export const BtnSecondary = ({ children, ...props }) => {
   );
 };
 
-export const BtnWhite = ({ children, ...props }) => {
+export const BtnWhite = ({ children, className = "", ...props }) => {
   return (
     <button
-      className="px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-transparent border-2 border-white hover:bg-white hover:text-yellow"
+      type={props.type || "button"}
+      className={`px-4 py-2 font-semibold tracking-widest text-white uppercase transition-colors duration-300 ease-in bg-transparent border-2 border-white hover:bg-white hover:text-yellow ${
+        props.disabled ? `cursor-not-allowed opacity-50` : ""
+      } ${className}`}
       {...props}
     >
       {children}
