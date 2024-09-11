76cdc56b8630088eeacfe7ec377e056377a3cae9
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-09-11 15:37:18 +0530

    changing color and font configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,11 @@ export default {
         light: "#f7f8f9",
         yellow: "#b79d82",
         white: "#ffffff",
+        lightGrey: "#ebebeb", // Updated
+        mediumGrey: "#7f7f7f", // Updated
+        darkGrey: "#a1a1a1", // Updated
+        darkerGrey: "#878787", // Updated
+        deepGrey: "#606060", // Updated
       },
       fontFamily: {
         sans: ["Open Sans", "sans-serif"],
