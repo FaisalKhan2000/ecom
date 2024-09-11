cc7ec096aaff37980309e19bbabc8c89a08dc373
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-09-11 15:46:25 +0530

    created components, pages , layouts and implemented routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/layout";
+import Home from "./pages/home";
+import Catalog from "./pages/catalog";
+import Cart from "./pages/cart";
+
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "catalog",
+          element: <Catalog />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 export default App;
